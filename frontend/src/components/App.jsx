02dd1258415ff3f97cb6aab11bcfc7fbf7f9edd9
clin155 <clin7985@gmail.com>
2022-07-15 21:30:51 -0700

    d

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,17 +1,15 @@
 import './Global.scss';
 import {
-  BrowserRouter,
-  Routes,
-  Route,
+  BrowserRouter,Routes,Route,Redirect,
 } from "react-router-dom";
+import {Home} from "./Home"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/">
-          {true ? }
-        </Route>
+        <Route path="/" 
+        element = {true ? <Home /> : <Redirect to="/login"/>}/>
       </Routes>
     </BrowserRouter>
   );
