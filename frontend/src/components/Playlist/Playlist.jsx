783fa8564f7cdb7eb952d007f6bdf72871bf2882
clin155 <clin7985@gmail.com>
2022-07-18 23:11:36 -0700

    c

--- frontend/src/components/Playlist/Playlist.jsx
+++ frontend/src/components/Playlist/Playlist.jsx
@@ -1,9 +1,16 @@
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function Playlist() {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <InfiniteScroll 
+                dataLength={items.length}
+            >
+                
+            </InfiniteScroll>
+        </Container>
     )
 }
---
+++ runner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -x
+set -Eeuo pipefail
+
+cd backend
+npm start
